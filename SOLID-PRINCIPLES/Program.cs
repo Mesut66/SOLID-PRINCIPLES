634159439c8890d538947ef5d803c0311adc6f72
mesut yiğit <genmes-66@hotmail.com>
2025-01-16 16:09:42 +0300

    program.cs empty

--- SOLID-PRINCIPLES/Program.cs
+++ SOLID-PRINCIPLES/Program.cs
@@ -1,13 +1,1 @@
-﻿using SOLID_PRINCIPLES._1_SingleResponsiblePrinciple.GoodDesign;
-using SOLID_PRINCIPLES._1_SingleResponsiblePrinciple.Model;
-
-SchoolModel model = new SchoolModel();
-
-model.Id = 1;
-model.Name = "Reşat Nuri";
-model.Address = "EyüpSultan";
-model.FoundingYear = DateTime.Now;
-
-School school = new School();
-
-school.AddSchool(model);
+﻿
